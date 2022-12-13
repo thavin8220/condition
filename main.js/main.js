05cf1c9eeffa828a1d67174252ef530e22ce3f96
thavin8220 <thavinmurugan@gmail.com>
2022-12-13 17:09:18 +0530

    add file

---
+++ main.js/main.js
@@ -0,0 +1,42 @@
+/*let a=prompt("Enter the number");
+if(0<a){
+    document.write("positive value" +a)
+}
+else{
+    document.write("negative value" +a)
+}*/
+
+/*let a=prompt("Enter The age");
+if(a<16){
+    document.write("child " +a)
+}
+
+else if(a<=50){
+    document.write("adult " +a)
+}
+else{
+    document.write("senior " +a)
+}*/
+
+/*let a=prompt("Enter the value");
+if(a%3==0 && a%5==0){
+    document.write("Valied number " +a);
+}
+else{
+    document.write("not valied number "+a)
+}*/
+
+/*let a=prompt("Enter the number");
+if(a%2==0){
+    document.write("Even number "+a);
+}
+else{
+    document.write("Odd number "+a)
+}*/
+
+let i=1;
+while(i<=5){
+    
+    document.write(i);
+    i++;
+}
